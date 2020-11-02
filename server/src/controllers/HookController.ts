7dd390514079f657a71cfcf22bb0d2c648a1b207
Alex Collin <alex@collin.su>
2020-11-02 03:48:15 +0300

    tokens

--- server/src/controllers/HookController.ts
+++ server/src/controllers/HookController.ts
@@ -1,7 +1,22 @@
 
 import * as express from 'express';
+import { Request, Response } from 'express';
+import { OpenViduService } from '../services/OpenViduService';
+import { OPENVIDU_URL, OPENVIDU_SECRET } from '../config';
 
 export const appHook = express.Router({
     strict: true
 });
 
+const openviduService = new OpenViduService();
+
+appHook.post('/', async (req: Request, res: Response) => {
+	let sessionId: string = req.body.sessionId;
+	console.log('Session ID received', req.body);
+	
+		const sessionResponse = await openviduService.createSession(sessionId, OPENVIDU_URL, OPENVIDU_SECRET);
+		sessionId =sessionResponse.id;
+	
+	
+	res.status(200).send(JSON.stringify({"status": "ok"}));
+});
--- server/src/services/OpenViduService.ts
+++ server/src/services/OpenViduService.ts
@@ -1,26 +1,47 @@
+import { Publisher } from 'openvidu-browser';
+import { Output } from '@angular/core';
 import { HttpClientService } from './HttpClientService';
+import { RecordingMode, OpenVidu, Recording, 
+    RecordingProperties, RecordingLayout, Session, 
+    SessionProperties, TokenOptions, OpenViduRole,
+    } from 'openvidu-node-client';
+import { OPENVIDU_URL, OPENVIDU_SECRET } from '../config';
+import { SessionOptions } from 'http2';
 
 export class OpenViduService {
 
-    private httpClientService: HttpClientService;
+    private : HttpClientService;
+    private api: OpenVidu;
 
 	constructor(){
-        this.httpClientService = new HttpClientService();
+        this.api = new OpenVidu(OPENVIDU_URL, OPENVIDU_SECRET);
     }
 
-	public async createSession(sessionId: string, openviduUrl: string, openviduSecret: string ): Promise<any> {
-        const url = openviduUrl + '/api/sessions';
-        console.log("Requesting session to ", url);
-        const body: string = JSON.stringify({ customSessionId: sessionId});
+    public async startRecording(sessionId: string): Promise<any> {
+        console.log("Start recording to ", sessionId);
+        var properties:RecordingProperties = {
+            outputMode: Recording.OutputMode.COMPOSED, 
+            recordingLayout: RecordingLayout.BEST_FIT,
+        };
 
-        return await this.httpClientService.post(body, url, openviduSecret);
+        return await this.api.startRecording(sessionId, properties);
+    }
+    
+	public async createSession(sessionId: string): Promise<Session> {
+        console.log("New session to ", sessionId);
+        var properties:SessionProperties = {
+            customSessionId: sessionId, 
+            recordingMode: RecordingMode.MANUAL,
+            defaultOutputMode: Recording.OutputMode.INDIVIDUAL
+        };
+        return await this.api.createSession (properties);
 	}
 
-	public async createToken(sessionId: string, openviduUrl: string, openviduSecret: string ): Promise<any> {
-		const url = openviduUrl + '/api/tokens';
-        console.log("Requesting token to ", url);
-        const body: string = JSON.stringify({ session: sessionId });
-
-        return await this.httpClientService.post(body, url, openviduSecret);
+	public async createToken(session: Session): Promise<string> {
+        console.log("New token to ", session.sessionId);
+        var properties:TokenOptions = {
+            role: OpenViduRole.PUBLISHER, 
+        };
+        return await session.generateToken(properties);
     }
 }
